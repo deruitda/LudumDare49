072c6562fdfb027992ffc06f1c294fb5ad6f7c3d
deruitda <deruitda@mail.gvsu.edu>
2021-10-02 22:27:15 -0400

    Added health bars, better projectile particles

--- Assets/Scripts/BaseMortal.cs
+++ Assets/Scripts/BaseMortal.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public abstract class BaseMortal : MonoBehaviour
 {
+    [SerializeField]
+    private HealthBar _healthBar;
+
     public float StartingHealth = 100;
     public float Health { get; protected set; }
 
     public IWeapon Weapon;
+
     private void Awake()
     {
         Health = StartingHealth;
+        _healthBar.SetHealthBarLevel(Health);
     }
 
     public void TakeDamage(float damage)
     {
         Health -= damage;
+        _healthBar.SetHealthBarLevel(Health);
         if(Health <= 0)
         {
             Die();
