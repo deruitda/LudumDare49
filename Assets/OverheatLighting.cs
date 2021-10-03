ff6bbff096ef04f894c2c29d5c8085e3c2bfbb8f
andrewmichienzi <34970861+amichie1@users.noreply.github.com>
2021-10-03 17:37:51 -0400

    adding halo

--- Assets/OverheatLighting.cs
+++ Assets/OverheatLighting.cs
@@ -1,38 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEditor;
 public class OverheatLighting : MonoBehaviour
 {
+
+
     [SerializeField]
     private ThunderBolt _bolt;
-    [SerializeField]
-    private Light _light;
-    [SerializeField]
-    private ParticleSystem _sparks;
-
-    [SerializeField]
-    private float _minIntensity = 0.1f;
-
-    [SerializeField]
-    private float _maxIntensity = 50f;
-
-    [SerializeField]
-    private float _minRange = 20f;
-
-    [SerializeField]
-    private float _maxRange = 200f;
-    [SerializeField]
-    private float _minLifetime = 0.2f;
-
-    [SerializeField]
-    private float _maxLifetime = 5f;
-    [SerializeField]
-    private float _minSpeed = 1f;
-
-    [SerializeField]
-    private float _maxSpeed = 30f;
-
 
     [SerializeField]
     private Color _coolColor = Color.yellow;
@@ -40,17 +15,18 @@ public class OverheatLighting : MonoBehaviour
     [SerializeField]
     private Color _overheatColor = Color.red;
 
+    [SerializeField]
     private Gradient gradient;
 
+    [SerializeField]
+    private float _minHaloSize = 0f;
+
+    [SerializeField]
+    private float _maxHaloSize = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        _light.intensity = _minIntensity;
-        var main = _sparks.main;
-        main.startSpeed = _minSpeed;
-        main.startLifetime = _minLifetime;
-        main.startColor = _coolColor;
 
         gradient = new Gradient();
         var colorKey = new GradientColorKey[2];
@@ -72,10 +48,7 @@ public class OverheatLighting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        setIntensity();
-        setLifetime();
-        setSpeed();
-        setColor();
+        SetColor();
     }
 
     private float GetMetricByIntensity(float min, float max, float overheatIntensity)
@@ -84,37 +57,16 @@ public class OverheatLighting : MonoBehaviour
         return (sub * overheatIntensity) + min;
     }
 
-    private void setIntensity()
+    private void SetColor()
     {
         float overheatMetric = _bolt.GetOverheatMetric();
-        float newIntensity = GetMetricByIntensity(_minIntensity, _maxIntensity, overheatMetric);
-
-        _light.intensity = Mathf.PingPong(_light.intensity, newIntensity);
-
-    }
-    private void setLifetime()
-    {
-        float overheatMetric = _bolt.GetOverheatMetric();
-        float newIntensity = GetMetricByIntensity(_minLifetime, _maxLifetime, overheatMetric);
-        var main = _sparks.main;
-
-        main.startLifetime = newIntensity;
-
-    }
-    private void setSpeed()
-    {
-        float overheatMetric = _bolt.GetOverheatMetric();
-        float newIntensity = GetMetricByIntensity(_minSpeed, _maxSpeed, overheatMetric);
-        var main = _sparks.main;
-        main.startSpeed = newIntensity;
-    }
-
-    private void setColor()
-    {
-        float overheatMetric = _bolt.GetOverheatMetric();
-        var main = _sparks.main;
         Color newColor = gradient.Evaluate(overheatMetric);
-        main.startColor = newColor;
-        _light.color = newColor;
+
+        SerializedObject halo = new SerializedObject(GetComponent("Halo"));
+        float size = GetMetricByIntensity(_minHaloSize, _maxHaloSize, overheatMetric);
+
+        halo.FindProperty("m_Size").floatValue = size;
+        halo.FindProperty("m_Color").colorValue = newColor;
+        halo.ApplyModifiedProperties();
     }
 }
